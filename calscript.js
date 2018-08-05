1404f4fbac5662eb6ac629b978c3f30b5c074346
Annie Kao <anniekao@trentu.ca>
2018-08-05 19:37:41 +0200

    changed the calculator layout; added functions to numbers so that they will be displayed

--- calscript.js
+++ calscript.js
@@ -33,8 +33,115 @@ function operate(x, y, op) {
   }
 }
 
-let x = 6;
-let y = 2;
-let op = "-"
+function display() {
+  disValue = "";
+  dis = document.getElementById("display");
+  let sub = document.getElementById("sub");
+  let multiply = document.getElementById("multiply");
+  let divide = document.getElementById("divide");
 
-operate(x,y,op)
+  clear.addEventListener("click", () => {
+    disValue = "";
+    dis.innerHTML = "0";
+  })
+
+  one.addEventListener("click", () => {
+    disValue += "1";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  two.addEventListener("click", () => {
+    disValue += "2";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  three.addEventListener("click", () => {
+    disValue += "3";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  four.addEventListener("click", () => {
+    disValue += "4";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  five.addEventListener("click", () => {
+    disValue += "5";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  six.addEventListener("click", () => {
+    disValue += "6";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  seven.addEventListener("click", () => {
+    disValue += "7";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  eight.addEventListener("click", () => {
+    disValue += "8";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  nine.addEventListener("click", () => {
+    disValue += "9";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  zero.addEventListener("click", () => {
+    disValue += "0";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  plus.addEventListener("click", () => {
+    disValue += "+";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  sub.addEventListener("click", () => {
+    disValue += "-";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  multiply.addEventListener("click", () => {
+    disValue += "*";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  divide.addEventListener("click", () => {
+    disValue += "/";
+    disValue = checkSize(disValue);
+    dis.innerHTML = disValue;
+  })
+
+  equal.addEventListener("click", () => {
+    console.log("result");
+  })
+}
+
+function checkSize(disValue){
+  disValue = disValue.toString();
+  let resetNum = "";
+  if (disValue.length > 12) {
+    resetNum = disValue[disValue.length-1];
+    return resetNum;
+    } return disValue;
+}
+
+
+display();
